c322a24a2600d6f0223396ad649d0c9528138145
Abdella <abdella@rangle.io>
2017-05-03 19:26:58 -0400

    finishes #144808471 - calendar added

---
+++ src/components/calendar.component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+
+BigCalendar.momentLocalizer(moment); // or globalizeLocalizer
+
+
+export default props => (
+  <BigCalendar
+    style={{height: '400px'}}
+    events={[]}
+/>);
