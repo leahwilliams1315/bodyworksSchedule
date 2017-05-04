c95a8ed36e210fec467e279ce9847326e6b182b9
leahwilliams1315 <leah1315@gmail.com>
2017-05-04 11:37:15 -0400

    [finishes #144807883] added treatment duration dropdown

--- src/components/sidemenu-left.component.js
+++ src/components/sidemenu-left.component.js
@@ -3,8 +3,10 @@ import {List} from 'material-ui/List';
 import Subheader from 'material-ui/Subheader';
 
 import MassageTypeDropdown from './drop-down-menu.component';
+import TreatmentDurationDropdown from './treatment-duration.component';
 
-export default ({massageTypes, massageTypeSelected, selectedMassageType}) => <List>
+export default ({massageTypes, massageTypeSelected, selectedMassageType, treatmentDuration, selectedDurationTime, durationSelected}) => <List>
   <Subheader>Select Your Massage Type</Subheader>
   <MassageTypeDropdown items={massageTypes} selectedItem={selectedMassageType} onMenuChange={massageTypeSelected}/>
+  <TreatmentDurationDropdown durations={treatmentDuration} selectedDuration={selectedDurationTime} onDurationChange={durationSelected} />
 </List>
