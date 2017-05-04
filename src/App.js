0ed25d4711536db75e7042177c4f98492d1d71d6
Abdella <abdellaalioncan@gmail.com>
2017-05-03 21:52:27 -0400

    [finishes #144831315] (#3)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import moment from 'moment';
 
 import Schedular from './components/calendar.component';
 
-
-
 class App extends Component {
+
+  state = {
+
+    allBookings: [{
+      'title': 'Massage With Sue',
+      'start': moment().startOf('day').hour(13).minute(0).toDate(),
+      'end': moment().startOf('day').hour(14).minute(0).toDate(),
+      desc: 'Sports massage with Sue',
+    }]
+
+  };
+
   render() {
     return (
       <div className="App">
-        <Schedular />
+        <Schedular bookings={this.state.allBookings} />
       </div>
     );
   }
